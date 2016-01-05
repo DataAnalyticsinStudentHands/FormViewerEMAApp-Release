2322a8de7167b4c2923b7b0ac59f5dec717b2b1f
Carl Steven <carl_steven@live.com>
2016-01-05 09:34:50 -0600

    Automate build via gulp. Reduce package.json

--- hooks/before_platform_add/add_plugins.js
+++ hooks/before_platform_add/add_plugins.js
@@ -19,7 +19,7 @@ var sys = require('sys')
 var exec = require('child_process').exec;
  
 function puts(error, stdout, stderr) {
-    sys.puts(stdout)
+    console.log(stdout)
 }
  
 pluginlist.forEach(function(plug) {
